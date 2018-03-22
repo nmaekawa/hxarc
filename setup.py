31917727de15d4acb5a1f0bc3b838b3857874c86
nmaekawa <nmaekawa@g.harvard.edu>
2018-03-22 15:56:38 -0400

    hxarc is pip-installable

---
+++ setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools import find_packages
+import os
+
+import hxarc
+VERSION = hxarc.__version__
+
+requirements = [
+    "Flask",
+    "Flask-Bcrypt",
+    "Flask-Caching",
+    "Flask-DebugToolbar",
+    "Flask-Login",
+    "Flask-Migrate",
+    "Flask-SQLAlchemy",
+    "flask-webpack",
+    "Flask-WTF",
+    "click",
+    "gunicorn",
+    "itsdangerous",
+    "Jinja2",
+    "MarkupSafe",
+    "python-dotenv",
+    "pyaml",
+    "SQLAlchemy",
+    "Werkzeug",
+    "WTForms"
+]
+
+test_requirements = [
+    "pytest",
+    "WebTest",
+    "factory-boy",
+]
+
+setup(
+    name="hxarc",
+    version=VERSION,
+    description="web version of https://github.com/Colin-Fredericks/hx-py/tree/master/XML_utilities",
+    author="nmaekawa",
+    author_email="nmaekawa@g.harvard.edu",
+    url="https://github.com/nmaekawa/hxarc",
+    packages=find_packages(exclude=["docs", "tests*"]),
+    package_dir={'hxarc': 'hxarc'},
+    include_package_data=True,
+    license="Apache 2.0",
+    keywords="hxpy",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Inteded audience :: Developers',
+        'License :: OSI Approved ++ Apache Sofware License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    install_requires=requirements,
+    tests_requires=test_requirements,
+    zip_safe=False,
+)
